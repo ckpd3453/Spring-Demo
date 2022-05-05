082b8e50b63696ce967924711b6556bec49a2e5b
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-05-05 16:05:35 +0530

    Applied and learned all the api calls method

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,22 +1,55 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import com.example.demo.entity.User;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * Contoller Class
+ */
 @RestController
 public class HelloController {
 
-    @RequestMapping("hello")
-    public ModelAndView sayHello() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("page.html");
-        return modelAndView;
-    }
-
-    @GetMapping("/chandra")
+    /**
+     * Get Method - To Read or Retrieve the data
+     * @return - String Type
+     * URL - http://localhost:8080/hello
+     */
+    @GetMapping("/hello")
     public String sayHelloBridgeLabz(){
         return "Hello From Bridgelabz";
+    }
+
+    /**
+     * Post Method - To define the parameters
+     * @param user - Body of the Reference Type
+     * @return - defined parameters in String type
+     * URL - http://localhost:8080/create-user
+     */
+    @PostMapping("/create-user")
+    public String createUser(@RequestBody User user){
+        return "User = "+ user.getFirstName()+" "+user.getLastName();
+    }
+
+    /**
+     * Get Method - To Read or Retrieve the data
+     * @param name - Parameter passed in the url
+     * @return - String Type
+     * URL - http://localhost:8080/hello/Chandrakant
+     */
+    @GetMapping("/hello/{name}")
+    public String sayHelloPathVariable(@PathVariable String name){
+        return "Hello "+ name;
+    }
+
+    /**
+     * PUT Method - To update the data
+     * @param firstName
+     * @param lastName
+     * @return - String Type
+     * URL -URL - http://localhost:8080/update/Chandrakant?lastName=Prasad
+     */
+    @PutMapping ("/update/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName){
+        return "Hello " + firstName + " " + lastName;
     }
 }
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Struct;
+
+/**
+ * Component Class
+ */
+@Component
+public class User {
+
+    /**
+     * Variables
+     */
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Getter And Setter Methods
+     * @return -Set Values
+     */
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
